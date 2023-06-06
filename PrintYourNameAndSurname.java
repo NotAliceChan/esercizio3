1f1fe462586cc6ae616587cb071041fafb59e9e0
Alice <fenuwu99@gmail.com>
2023-06-06 16:36:55 +0200

    esercizio 3

---
+++ PrintYourNameAndSurname.java
@@ -0,0 +1,10 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+
+        String name = "Alice";
+        String surname = "Barretta";
+        System.out.println( name + " " + surname);
+
+
+    }
+}
